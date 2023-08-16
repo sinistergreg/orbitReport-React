66c06f0722925d9d881f375fe5a4cea9b8d13494
Greg <138933807+sinistergreg@users.noreply.github.com>
2023-08-16 08:53:06 -0600

    Assignment#6 DONE

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import './styling.css';
+
+function Banner(){
+    return(
+        <div>
+            <div>
+       <h1 className="header">Orbit Report</h1>
+       <p>Click on the buttons to see the satellites in that orbit type</p>
+       </div>
+       </div>
+    );
+ }
+ 
+ export default Banner;
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,21 @@
-const Buttons = () => {
+import './styling.css';
+import satData from './satData';
+
+const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    <div className="flex-container"> 
+      {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      ))}
+      
+      <button onClick={() => setSat(satData)}> {/* Reset to all orbits */}
+        All Orbits
+      </button>
+    </div>
   );
 };
+  const displaySats = ['Low', 'Medium', 'high']; // Example array of satellite types
 
 export default Buttons;
